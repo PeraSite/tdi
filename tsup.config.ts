fab8061a7a6d4ad8afa41c7eacadb33f1524bc5e
PeraSite <pretocki3@gmail.com>
2024-12-30 21:11:55 +0900

    feat: edit package setting

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
     entry: ['src/index.ts'],
     format: ["esm", "cjs"],
-    sourcemap: true,
     clean: true,
     minify: true,
+    dts: true,
 })
